1c52d1de1222b9a1f343f5a48da3bba4d8171857
Steffen70 <steffen@seventy.mx>
2024-07-30 19:03:41 +0200

    Added flake package output.

--- README.md
+++ README.md
@@ -15,3 +15,17 @@ cd ./public
 # Start the PHP built-in server
 php -S localhost:$env:PHP_INTERFACE_PORT
 ```
+
+## Building the Project with Nix
+
+To build the project using Nix, you can run the following command:
+
+```sh
+nix build --option sandbox false
+```
+
+### Note:
+
+Disabling the sandbox is necessary for this build because the `php` command needs network access to download Composer packages.
+While this approach works for now, we are exploring more elegant solutions to handle dependencies in a sandboxed environment.
+This includes pre-fetching dependencies or using local caches to ensure a secure and reproducible build process.
--- flake.nix
+++ flake.nix
@@ -20,30 +20,69 @@
   };
 
   outputs =
-    { self, ... }@inputs:
+    { self, base_flake, ... }@inputs:
     inputs.flake-utils.lib.eachDefaultSystem (
       system:
       let
         unstable = import inputs.nixpkgs { inherit system; };
 
-        baseDevShell = inputs.base_flake.outputs.devShell.${system};
+        pname = "php_interface";
+        version = "${base_flake.majorMinorVersion.${system}}.0";
+
+        baseDevShell = base_flake.devShell.${system};
 
         # Create a custom PHP build with the grpc extension and custom configuration
         myPhp = unstable.php.withExtensions (exts: [
           unstable.php.extensions.grpc
           unstable.php.extensions.protobuf
         ]);
+
+        dependencies = baseDevShell.buildInputs ++ [
+          unstable.protobuf
+          unstable.grpc # C based gRPC
+          myPhp
+          unstable.php83Packages.composer # PHP dependency manager
+        ];
       in
       {
         devShell = unstable.mkShell {
-          buildInputs = baseDevShell.buildInputs ++ [
-            unstable.protobuf
-            unstable.grpc # C based gRPC
-            myPhp
-            unstable.php83Packages.composer # PHP dependency manager
-          ];
-
+          buildInputs = dependencies;
           shellHook = baseDevShell.shellHook;
+        };
+
+        packages.default = unstable.stdenv.mkDerivation {
+          pname = pname;
+          version = version;
+
+          src = ./.;
+
+          buildInputs = dependencies;
+
+          buildPhase = ''
+            # Set environment variables for compilation
+            export PROTOBUF_PATH=${base_flake.protos.${system}}
+
+            export PSModulePath="${base_flake.powershell_modules.${system}}"
+
+            # Install the PHP dependencies
+            composer install
+
+            pwsh -Command "& {
+              Import-Module GrpcGenerator
+
+              # Generate the gRPC client and server code from the protos
+              Update-PhpGrpc -ProtosArray @('model', 'playing_field')
+            }"
+
+            # Copy all .php files + vendor/ to the output directory while preserving the directory structure
+            rsync -av --include='*/' --include='*.php' --include='LICENSE' --include='vendor/***' --exclude='*' . $out
+          '';
+
+          meta = with inputs.nixpkgs.lib; {
+            description = "The main service of the Game Theory Demo Application.";
+            license = licenses.mpl20;
+            maintainers = with maintainers; [ steffen70 ];
+          };
         };
       }
     );
